007cc7fa258cd57712d5b92c7b2b9cf344ceface
megantebro <dazaizhongcun861@gmail.com>
2025-02-24 15:44:56 +0900

    jsonで設定変えられるようにした

--- cpsreduce.py
+++ cpsreduce.py
@@ -1,8 +1,17 @@
 from pynput import mouse
 import time
+import json
+
+
+with open("./config.json","r") as file:
+    json_data = json.load(file)
+
+
 
 last_clicks = []
-MAX_CLICKS = 15
+MAX_CLICKS = int(json_data["max_clicks"] / (1 / json_data["update_speed"]))
+UPDATE_SPEED = json_data["update_speed"]
+print(MAX_CLICKS)
 
 while True:
     def on_click(x, y, button, pressed):
@@ -14,7 +23,7 @@ while True:
         current_time = time.time()
         
         # 1秒以上経過したクリックを削除
-        last_clicks = [t for t in last_clicks if current_time - t <= 1]
+        last_clicks = [t for t in last_clicks if current_time - t <= UPDATE_SPEED]
         
         if len(last_clicks) < MAX_CLICKS:
             last_clicks.append(current_time)
@@ -22,6 +31,7 @@ while True:
         else:
             listener.suppress_event()
             return True
+        
 
     # マウス入力を監視するリスナーを作成
     with mouse.Listener(on_click=on_click) as listener:
